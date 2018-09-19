e841e0705411efe847e72ef3614765b68e2c084d
ErinSmith17 <erin7513@gmail.com>
2018-09-19 08:33:25 -0400

    update

--- Dictionary Project_ErinSmith.py
+++ Dictionary Project_ErinSmith.py
@@ -7,37 +7,37 @@ pastries = []
 orders = cakes + cupcakes + pastries
 order = input("Would you like to place an order?: ")
 
+
 if order == "no":
     print("Thank you, Have a nice day!")
 else:
-    print("For a cake use the first function, for a cupcake use the second function, and for a pastry use the third function.")
-    
-    def first(shape,size,flavor):
-        cakes = {
-            'shape' : shape,
-            'size' : size,
-            'flavor' : flavor
-            }
-        cakes.append(cakes)
-        return "Your order %s has been placed" % cakes
+    answer = input("For a cake use the first function, for a cupcake use the second function, and for a pastry use the third function.")
+    while answer == "yes":  
+        def first(shape,size,flavor):
+            cakes = {
+                'shape' : shape,
+                'size' : size,
+                'flavor' : flavor
+                }
+            cakes.append(cakes)
+            return "Your order %s has been placed" % cakes
 
-    def second(size,flavor,frosting):
-        cupcakes = {
-            'size' : size,
-            'flavor' : flavor,
-            'frosting' : frosting
-            }
-        cupcakes.append(cupcakes)
-        return "Your order %s has been placed" % cupcakes
+        def second(size,flavor,frosting):
+            cupcakes = {
+                'size' : size,
+                'flavor' : flavor,
+                'frosting' : frosting
+                }
+            cupcakes.append(cupcakes)
+            return "Your order %s has been placed" % cupcakes
 
-    def third(name,flavor,topping):
-        pastry = {
-            'name' : name,
-           'flavor' : flavor,
-            'topping' : topping
-            }
-        pastries.append(pastry)
-        return "Your order 5s has been placed" % pastry
-else:
-    print("Your order is %s") % orders 
+        def third(name,flavor,topping):
+            pastry = {
+                'name' : name,
+               'flavor' : flavor,
+                'topping' : topping
+                }
+            pastries.append(pastry)
+            return "Your order 5s has been placed" % pastry
 
+#it isn't letting the user input an order
