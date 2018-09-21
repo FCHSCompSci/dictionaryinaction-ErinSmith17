633650d5f23e8d73c96c878315b020b93f943b09
ErinSmith17 <erin7513@gmail.com>
2018-09-21 08:32:14 -0400

    update

--- Dictionary Project_ErinSmith.py
+++ Dictionary Project_ErinSmith.py
@@ -1,6 +1,7 @@
 #Erin Smith -
 #Dictionary Project 9/17/18
 
+order =[]
 def make_cake(c_id,size,shape,flavor):
     cake = {
         'c_id' : cake_id,
@@ -23,13 +24,19 @@ while True:
     user_input = input("Would you like to order a [c]ake,[p]ickup or [e]xit? ")
     if user_input == "c":
         order_name = input("What is the name for the order? ")
-        customer(name).append(order_name)
+        order_name == make_customer(cake_id,'name')
         cake_id = cake_id + 1
-        cake_size = input("What size would you like? ")
-        cake_shape = input("What shape of cake would you like? ")
-        cake_flavor = input("What flavor of cake would you like? ")
+        order_size = input("What size would you like? ")
+        order_size == make_cake('size')
+        order_shape = input("What shape of cake would you like? ")
+        order_shape == make_cake('shape')
+        order_flavor = input("What flavor of cake would you like? ")
+        order_flavor == make_cake('flavor')
     elif user_input == "p":
         pass
     elif user_input == "e":
         print("Have a nice day!")
         break
+#need help figuring out how to append each input to the dictionary
+#every time i put order_size == make_cake('size') it says that the
+#other positional arguments are missing but im doing the other requirement below
